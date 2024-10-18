669604b373ad9a079e4000769d015e05ae7181a1
lavish gehlod <lavish>
2024-10-19 01:49:05 +0530

    sd

--- src/pages/admin/pages/AddProduct.jsx
+++ src/pages/admin/pages/AddProduct.jsx
@@ -157,7 +157,7 @@ function AddProduct() {
                         type="text"
                         placeholder="key"
                         value={key}
-                        onChange={(e) => setkey(e.target.value)}
+                        onChange={(e) => setKey(e.target.value)}
                         className="w-full px-3 py-2 text-base text-gray-700 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600"
                     />
                     <input
